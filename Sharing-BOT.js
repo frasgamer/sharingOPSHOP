06a60b8cca9ad914df44864c2d0c493802ac98e6
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-20 21:52:08 +0300

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,28 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+~~
+OP SHOP يرحب بكم :dove:
+~~
+ممكن صدفة تخلق صحبة
+تنورنا يالغالي بسيرفرك 
+السيرفر عبارة عن شوب وانا امنيتي انجح 
+لإااتمنى تدخل ياقلبي :heart:
+                               [ رابط السيرفر ] 
+الدعووة خاصة لك ياقلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+client.login(process.env.BOT_TOKEN);
+
+//A7MD.login('التوكين حقك');
